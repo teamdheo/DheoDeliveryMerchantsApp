2e356d952e198c5f4b67750c3dfabf5c838df167
robi042 <34013030+robi042@users.noreply.github.com>
2020-09-19 21:24:55 +0600

    Update SingleAddressPickupSlotActivity.java

--- app/src/main/java/com/example/myapplication/SingleAddressPickupSlotActivity.java
+++ app/src/main/java/com/example/myapplication/SingleAddressPickupSlotActivity.java
@@ -1,17 +1,20 @@
 package com.example.myapplication;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.myapplication.modelClassAvaiablePickupSlot.AvailablePickupSlot;
 import com.example.myapplication.modelClassAvaiablePickupSlot.M;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 import es.dmoral.toasty.Toasty;
