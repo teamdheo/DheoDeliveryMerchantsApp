f224a4b27b47867f51da56b0a41ea24ed0236d71
robi042 <34013030+robi042@users.noreply.github.com>
2020-09-19 21:08:42 +0600

    Update ClientDashboardActivity.java
    dashboard activity

--- app/src/main/java/com/example/myapplication/ClientDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/ClientDashboardActivity.java
@@ -1,18 +1,25 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.myapplication.ModelClassAssingedCourierInfoDashboard.AssingedCourierInfoDashboard;
 import com.example.myapplication.modelClassPickupAddresses.M;
 import com.example.myapplication.modelClassPickupAddresses.PickupAddresses;
 import com.squareup.picasso.Picasso;
@@ -29,16 +36,19 @@ public class ClientDashboardActivity extends AppCompatActivity {
     SQLiteDatabase sqLiteDatabase;
     private int session = 0;
     private String phone;
-    private String photoUrl;
+    private String photoUrl, scooter_url;
     private int clientId;
     private String password;
-    private ImageView profile_photo;
+    private ImageView profile_photo, scooter;
     private TextView client_name, total_balance;
     private String photo_url;
     private String name;
     private int balance;
-    private Button request_pickup;
+    private Button request_pickup, next_pickup;
     private List<M> pickup_address_length;
+    private List<com.example.myapplication.ModelClassAssingedCourierInfoDashboard.M> pickup_info_dashboard;
+    private RecyclerView pickup_list;
+    private RecyclerView.Adapter adapter;
     Helper helper = new Helper(this);
 
 
@@ -50,21 +60,27 @@ public class ClientDashboardActivity extends AppCompatActivity {
         client_name = (TextView) findViewById(R.id.name);
         total_balance = (TextView) findViewById(R.id.amount) ;
         request_pickup =(Button) findViewById(R.id.pickup_request);
-        //check = (TextView) findViewById(R.id.check);
-
+        next_pickup =(Button) findViewById(R.id.Next_pickups);
+        scooter = (ImageView) findViewById(R.id.scooter);
+        pickup_list = (RecyclerView) findViewById(R.id.recycler_pickup_agent);
+        pickup_list.setHasFixedSize(true);
+        pickup_list.setLayoutManager(new LinearLayoutManager(this));
+        next_pickup.setVisibility(View.GONE);
+        scooter.setVisibility(View.GONE);
+        getSupportActionBar().setElevation(0);//remove actionbar shadow
+        setTitle("My Dashboard");
         try{
-            if(helper.getPhoto_Url() != "default.svg"){
+            if(helper.getPhoto_Url().equals("default.svg")){
+                profile_photo =(ImageView) findViewById(R.id.profile_photo);
+            }
+            else {
                 profile_photo =(ImageView) findViewById(R.id.profile_photo);
                 photo_url = "https://dheo-static-sg.s3-ap-southeast-1.amazonaws.com/img/rocket/clients/" + helper.getPhoto_Url() ;
                 Picasso.get().load(photo_url).into(profile_photo);
             }
-            else {
-                profile_photo =(ImageView) findViewById(R.id.profile_photo);
-            }
-        }catch (SQLiteException e){
+        }catch (SQLException e){
 
         }
-
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             phone = extras.getString("PHONE");
@@ -102,6 +118,50 @@ public class ClientDashboardActivity extends AppCompatActivity {
             startActivity(i);
         }
         query.close();
+
+        Call<AssingedCourierInfoDashboard> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .assigned_agent_info_dashboard(clientId);
+
+        call.enqueue(new Callback<AssingedCourierInfoDashboard>() {
+            @Override
+            public void onResponse(Call<AssingedCourierInfoDashboard> call, Response<AssingedCourierInfoDashboard> response) {
+                try {
+                    AssingedCourierInfoDashboard pickup = response.body();
+                    pickup_info_dashboard = pickup.getM();
+                    if(pickup_info_dashboard.size() > 0){
+
+                        next_pickup.setVisibility(View.VISIBLE);
+                        scooter.setVisibility(View.VISIBLE);
+                        scooter_url= "https://dheo-static-sg.s3-ap-southeast-1.amazonaws.com/img/scooter.png";
+                        Picasso.get().load(scooter_url).into(scooter);
+                        next_pickup.setText("Next pickups");
+                        request_pickup.setText("Cancel or Change");
+                        Animation animation = new AlphaAnimation((float) 0.5, 0);
+                        animation.setDuration(1000);
+                        animation.setInterpolator(new LinearInterpolator());
+                        animation.setRepeatCount(Animation.INFINITE);
+                        animation.setRepeatMode(Animation.REVERSE);
+                        scooter.startAnimation(animation);
+                    }
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_LONG).show();
+                }
+                adapter = new AdapterClasspickupInfoDashboard(pickup_info_dashboard, getApplicationContext());
+                pickup_list.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<AssingedCourierInfoDashboard> call, Throwable t) {
+                Toasty.error(getApplicationContext(), "স্লো ইন্টারনেটঃ আবার চেস্টা করুন!", Toast.LENGTH_LONG, true).show();
+                Intent i = new Intent(getApplicationContext(), ClientDashboardActivity.class);
+                startActivity(i);
+            }
+
+        });
+
         request_pickup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -143,6 +203,15 @@ public class ClientDashboardActivity extends AppCompatActivity {
 
 //                Intent i = new Intent(ClientDashboardActivity.this,ListActivityPickupAddress.class);
 //                startActivity(i);
+            }
+        });
+        total_balance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ClientDashboardBillingActivity.class);
+                intent.putExtra("name_c", name);
+                intent.putExtra("balance_c", balance);
+                startActivity(intent);
             }
         });
     }
