26ff11c5de047e8d19b5b7e5da5e3954d4ff5fba
robi042 <34013030+robi042@users.noreply.github.com>
2020-09-28 14:57:42 +0600

    Create M.java
    model class

---
+++ app/src/main/java/com/example/myapplication/ModelClassClientEditPayload/M.java
@@ -0,0 +1,32 @@
+
+package com.example.myapplication.ModelClassClientEditPayload;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class M {
+
+    @SerializedName("editable_amount")
+    @Expose
+    private String editableAmount;
+    @SerializedName("ediable_number")
+    @Expose
+    private String ediableNumber;
+
+    public String getEditableAmount() {
+        return editableAmount;
+    }
+
+    public void setEditableAmount(String editableAmount) {
+        this.editableAmount = editableAmount;
+    }
+
+    public String getEdiableNumber() {
+        return ediableNumber;
+    }
+
+    public void setEdiableNumber(String ediableNumber) {
+        this.ediableNumber = ediableNumber;
+    }
+
+}
