0f14a6a0d6bf54944805de36d09dde884c25073e
robi042 <34013030+robi042@users.noreply.github.com>
2020-09-29 18:09:31 +0600

    Update SettingsActivity.java
    settings activity

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -2,13 +2,40 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.SQLException;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class SettingsActivity extends AppCompatActivity {
+    TextView setting_name,go_back;
+    ImageView setting_dp;
+    private String photo_url;
+    Helper helper = new Helper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setElevation(0);//remove actionbar shadow
+        setTitle("Account Settings");
         setContentView(R.layout.activity_settings);
+        setting_name = findViewById(R.id.setting_name);
+        go_back = findViewById(R.id.go_back);
+        setting_name.setText(helper.getName());
+        try{
+            if(helper.getPhoto_Url().equals("default.svg")){
+                setting_dp = findViewById(R.id.setting_profile_photo);
+            }
+            else {
+                setting_dp = findViewById(R.id.setting_profile_photo);
+                photo_url = "https://dheo-static-sg.s3-ap-southeast-1.amazonaws.com/img/rocket/clients/" + helper.getPhoto_Url() ;
+                Picasso.get().load(photo_url).into(setting_dp);
+            }
+        }catch (SQLException e){
+
+        }
+        go_back.setText("< Go Back");
     }
 }
