9fd9df7f9d4590a0d5dd04d1a5e35e4d6d03f3c7
robi042 <34013030+robi042@users.noreply.github.com>
2020-09-30 20:30:29 +0600

    Update SettingsActivity.java
    setting

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -1,18 +1,41 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.SQLException;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.myapplication.ModelClassBanksAndBranches.BanksAndBranches;
+import com.example.myapplication.ModelClassBanksAndBranches.M;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SettingsActivity extends AppCompatActivity {
+    ArrayList<String> bank_name;
+    ArrayList<String> branches_name;
+    ArrayAdapter<String> arrayAdapter;
     TextView setting_name,go_back;
     ImageView setting_dp;
+    private Spinner bank_name_show;
     private String photo_url;
+    private Button bank, other_option, cash, bkash, nagad;
+    LinearLayout linearLayout;
     Helper helper = new Helper(this);
 
     @Override
@@ -23,6 +46,13 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         setting_name = findViewById(R.id.setting_name);
         go_back = findViewById(R.id.go_back);
+        bank = findViewById(R.id.bank);
+        other_option = findViewById(R.id.other_option);
+        bank_name_show = findViewById(R.id.bank_name);
+//        cash = findViewById(R.id.cash);
+//        bkash = findViewById(R.id.bkash);
+//        nagad = findViewById(R.id.nagad);
+        //linearLayout = findViewById(R.id.linear_address);
         setting_name.setText(helper.getName());
         try{
             if(helper.getPhoto_Url().equals("default.svg")){
@@ -37,5 +67,33 @@ public class SettingsActivity extends AppCompatActivity {
 
         }
         go_back.setText("< Go Back");
+        bank_name = new ArrayList<>();
+
+        //linearLayout.setBackgroundColor(Color.argb(0,0,0, (float) 0.2));
+        Call<BanksAndBranches> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .bank_and_branches();
+        call.enqueue(new Callback<BanksAndBranches>() {
+            @Override
+            public void onResponse(Call<BanksAndBranches> call, Response<BanksAndBranches> response) {
+                if(response.body() != null){
+                    List<M> s = response.body().getM();
+                    for(int i = 0; i<s.size(); i++){
+                        bank_name.add(s.get(i).getBankName());
+
+
+                    }
+
+                }
+            }
+
+
+            @Override
+            public void onFailure(Call<BanksAndBranches> call, Throwable t) {
+
+            }
+        });
+
     }
 }
