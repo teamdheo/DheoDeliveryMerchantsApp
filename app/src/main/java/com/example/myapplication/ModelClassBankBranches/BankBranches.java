8eef9d4996628fa91b20a5f35d6aa34528798873
robi042 <34013030+robi042@users.noreply.github.com>
2020-10-03 11:40:59 +0600

    Create BankBranches.java

---
+++ app/src/main/java/com/example/myapplication/ModelClassBankBranches/BankBranches.java
@@ -0,0 +1,32 @@
+
+package com.example.myapplication.ModelClassBankBranches;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class BankBranches {
+
+    @SerializedName("e")
+    @Expose
+    private Integer e;
+    @SerializedName("m")
+    @Expose
+    private M m;
+
+    public Integer getE() {
+        return e;
+    }
+
+    public void setE(Integer e) {
+        this.e = e;
+    }
+
+    public M getM() {
+        return m;
+    }
+
+    public void setM(M m) {
+        this.m = m;
+    }
+
+}
