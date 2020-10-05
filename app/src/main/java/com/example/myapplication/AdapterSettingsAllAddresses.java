f35b5d98a09df6dfcfc87328c1cac98e3b751dcf
robi042 <34013030+robi042@users.noreply.github.com>
2020-10-05 16:37:24 +0600

    Update AdapterSettingsAllAddresses.java
    settings all pick adapter

--- app/src/main/java/com/example/myapplication/AdapterSettingsAllAddresses.java
+++ app/src/main/java/com/example/myapplication/AdapterSettingsAllAddresses.java
@@ -1,30 +1,42 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.modelClassPickupAddresses.M;
 
+import java.io.IOException;
 import java.util.List;
+
+import es.dmoral.toasty.Toasty;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterSettingsAllAddresses extends RecyclerView.Adapter<AdapterSettingsAllAddresses.AddressViewHolder> {
     private List<M> pickup_address;
     private Context mycontex;
     Helper helper;
     String address_id_check;
+    private ProgressDialog progressDialog;
 
     public AdapterSettingsAllAddresses(List<M> pickup_address, Context mycontex) {
         this.pickup_address = pickup_address;
         this.mycontex = mycontex;
         Helper helper = new Helper(mycontex);
+        progressDialog = new ProgressDialog(mycontex);
     }
     @NonNull
     @Override
@@ -35,10 +47,48 @@ public class AdapterSettingsAllAddresses extends RecyclerView.Adapter<AdapterSet
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AddressViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull AddressViewHolder holder, final int position) {
         holder.address_id.setText(pickup_address.get(position).getAddress_id());
         holder.setting_Address.setText(pickup_address.get(position).getClientPickupAddress());
         holder.setting_phone.setText(pickup_address.get(position).getPhone_no());
+        holder.delete_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.setMessage("Removing...");
+                progressDialog.show();
+                Call<ResponseBody> call = RetrofitClient
+                        .getInstance()
+                        .getApi()
+                        .delete_pickup_address(pickup_address.get(position).getAddress_id());
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        String s = null;
+                        try {
+                            s = response.body().string();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(mycontex, s, Toast.LENGTH_LONG).show();
+                        if (s.equals("{\"e\":0}")){
+                            progressDialog.dismiss();
+                            Toasty.error(mycontex, "successfully removed", Toast.LENGTH_LONG, true).show();
+                            Intent intent = new Intent(mycontex, SettingsActivity.class);
+                            mycontex.startActivity(intent);
+
+                        }
+                        else{
+                            Toasty.error(mycontex, "server failed to response", Toast.LENGTH_LONG, true).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toasty.error(mycontex, "Try Again!", Toast.LENGTH_LONG, true).show();
+                    }
+                });
+            }
+        });
 
     }
 
