8215679cec217e335a103c2d8aeaea88b2353f12
robi042 <34013030+robi042@users.noreply.github.com>
2020-10-13 11:36:17 +0600

    Create ClientPayloadSearch.java

---
+++ app/src/main/java/com/example/myapplication/ModelClassClientPayloadSearch/ClientPayloadSearch.java
@@ -0,0 +1,33 @@
+
+package com.example.myapplication.ModelClassClientPayloadSearch;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ClientPayloadSearch {
+
+    @SerializedName("e")
+    @Expose
+    private Integer e;
+    @SerializedName("m")
+    @Expose
+    private List<M> m = null;
+
+    public Integer getE() {
+        return e;
+    }
+
+    public void setE(Integer e) {
+        this.e = e;
+    }
+
+    public List<M> getM() {
+        return m;
+    }
+
+    public void setM(List<M> m) {
+        this.m = m;
+    }
+
+}
