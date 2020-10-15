9a5954a726139dd8a7404d8a987e87e2032a6806
robi042 <34013030+robi042@users.noreply.github.com>
2020-10-15 11:07:07 +0600

    Create Agent.java

---
+++ app/src/main/java/com/example/myapplication/ModelClassPickupMapInfo/Agent.java
@@ -0,0 +1,65 @@
+
+package com.example.myapplication.ModelClassPickupMapInfo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Agent {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("photo")
+    @Expose
+    private String photo;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("ping")
+    @Expose
+    private Ping ping;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Ping getPing() {
+        return ping;
+    }
+
+    public void setPing(Ping ping) {
+        this.ping = ping;
+    }
+
+}
