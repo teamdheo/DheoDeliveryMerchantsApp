d9d7596c62feb018fc94ae36948daec5f54a852d
robi042 <34013030+robi042@users.noreply.github.com>
2020-10-21 17:15:25 +0600

    Create M.java

---
+++ app/src/main/java/com/example/myapplication/ModelClassTrackerLogEntry/M.java
@@ -0,0 +1,54 @@
+
+package com.example.myapplication.ModelClassTrackerLogEntry;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class M {
+
+    @SerializedName("entry_id")
+    @Expose
+    private Integer entryId;
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("time")
+    @Expose
+    private String time;
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    public Integer getEntryId() {
+        return entryId;
+    }
+
+    public void setEntryId(Integer entryId) {
+        this.entryId = entryId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
