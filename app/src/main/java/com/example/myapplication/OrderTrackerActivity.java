4c5f4e7fa2574cffd5163f098e8f24f0cdf2b844
robi042 <34013030+robi042@users.noreply.github.com>
2020-10-22 21:32:05 +0600

    order tracker activity

--- app/src/main/java/com/example/myapplication/OrderTrackerActivity.java
+++ app/src/main/java/com/example/myapplication/OrderTrackerActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.database.SQLException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -15,6 +17,9 @@ import com.example.myapplication.ModelClassTrackerLogEntry.M;
 import com.example.myapplication.ModelClassTrackerLogEntry.TrackerLogEntry;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.List;
 
 import retrofit2.Call;
@@ -22,10 +27,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class OrderTrackerActivity extends AppCompatActivity {
-    private String short_id, client_name, photo_url;
-    private int payload_id;
-    private TextView track_client_name,order_no;
-    private ImageView track_client_image;
+    private String short_id, client_name, photo_url,label_image_url;
+    private int payload_id,day;
+    private TextView track_client_name,order_no,friday_note, dhep_delivery, the_user_manual, meet_the_team, privacy_policy;
+    private ImageView track_client_image,label_image;
     private RecyclerView tracker_events;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -35,10 +40,17 @@ public class OrderTrackerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setElevation(0);//remove actionbar shadow
+        setTitle("Order tracker page");
         setContentView(R.layout.activity_order_tracker);
         tracker_events = findViewById(R.id.tracker_events);
         track_client_name = findViewById(R.id.tracking_name);
         order_no = findViewById(R.id.order_no);
+        friday_note = findViewById(R.id.friday_note);
+        dhep_delivery = findViewById(R.id.billing_dheo_delivery);
+        the_user_manual = findViewById(R.id.billing_The_manual);
+        meet_the_team = findViewById(R.id.billing_meet_team);
+        privacy_policy = findViewById(R.id.billing_policy);
         layoutManager = new LinearLayoutManager(this);
         tracker_events.setLayoutManager(layoutManager);
         Bundle extras = getIntent().getExtras();
@@ -47,8 +59,14 @@ public class OrderTrackerActivity extends AppCompatActivity {
             payload_id = extras.getInt("payload_id");
             client_name = extras.getString("client_name");
         }
+        friday_note.setVisibility(View.GONE);
         track_client_name.setText(client_name);
         order_no.setText("Order number: "+helper.getClientId()+"."+payload_id);
+        day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        if(day == 6){
+            friday_note.setVisibility(View.VISIBLE);
+        }
+        //Toast.makeText(getApplicationContext(), day+"", Toast.LENGTH_LONG).show();
         try {
             if (helper.getPhoto_Url().equals("default.svg")) {
                 track_client_image = (ImageView) findViewById(R.id.tracker_profile_photo);
@@ -58,6 +76,14 @@ public class OrderTrackerActivity extends AppCompatActivity {
                 Picasso.get().load(photo_url).into(track_client_image);
             }
         } catch (SQLException e) {
+
+        }
+        try {
+            label_image = findViewById(R.id.label_image);
+            label_image_url = "https://dheo-static.s3.ap-south-1.amazonaws.com/img/intakes/" +short_id + ".jpg";
+            Picasso.get().load(label_image_url).into(label_image);
+
+        } catch (NullPointerException e) {
 
         }
 
@@ -74,7 +100,7 @@ public class OrderTrackerActivity extends AppCompatActivity {
                    if (trackerLogEntry.getM().size()<1){
                        tracker_events.setVisibility(View.GONE);
                    }
-                   Toast.makeText(getApplicationContext(), trackerLogEntry.getM().size() +"", Toast.LENGTH_LONG).show();
+
                }catch (NullPointerException e){}
                adapter = new AdapterTrackerEvents(all_log_entries,getApplicationContext());
                tracker_events.setAdapter(adapter);
@@ -83,6 +109,43 @@ public class OrderTrackerActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<TrackerLogEntry> call, Throwable t) {
 
+            }
+        });
+        dhep_delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ClientDashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+        the_user_manual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://rocket.dheo.com/user-manual";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+        meet_the_team.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://team.dheo.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+        privacy_policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://dheo.com/privacy";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
             }
         });
     }
