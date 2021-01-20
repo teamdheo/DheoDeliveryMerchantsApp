a71cd8a62da43626bd051d99b82a39d7c398dd4f
robi042 <34013030+robi042@users.noreply.github.com>
2021-01-20 16:36:10 +0600

    pagination and footer for order creation activity

--- app/src/main/java/com/dheo/dheodeliverymerchantapp/PickupEntryActivity.java
+++ app/src/main/java/com/dheo/dheodeliverymerchantapp/PickupEntryActivity.java
@@ -5,12 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dheo.dheodeliverymerchantapp.ModelClassPickupHistory.PickupHistory;
@@ -28,6 +31,7 @@ import retrofit2.Response;
 public class PickupEntryActivity extends AppCompatActivity {
     private Button order_create_btn,cancel_entry_btn,save_entry_btn,old_pickup,new_pickup;
     private DatePicker entry_datePicker;
+    private TextView phone_call, facebook, my_delivery, dashboard_billing, settings, user_manual, log_out, dhep_delivery, the_user_manual, meet_the_team, privacy_policy;
     private EditText entry_customer_name,entry_customer_address,entry_customer_phone,entry_customer_cod;
     private String month,date;
     private int month_of_year, client_id, page_number =1, load_pickup_remaining;
@@ -35,8 +39,10 @@ public class PickupEntryActivity extends AppCompatActivity {
     private RecyclerView.Adapter load_pickup_adapter;
     private RecyclerView.LayoutManager load_pickup_layout_manager;
     private List<M> all_list_of_orders;
-    LinearLayout order_entry_layout;
+    LinearLayout order_entry_layout,recycle_layout_view;
     Helper helper = new Helper(this);
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +51,7 @@ public class PickupEntryActivity extends AppCompatActivity {
         save_entry_btn = findViewById(R.id.save_entry_btn);
         cancel_entry_btn = findViewById(R.id.cancel_entry_btn);
         order_entry_layout = findViewById(R.id.order_entry_layout);
+        recycle_layout_view = findViewById(R.id.recycle_layout_view);
         entry_datePicker = findViewById(R.id.entry_datePicker);
         entry_customer_name = findViewById(R.id.entry_customer_name);
         entry_customer_address = findViewById(R.id.entry_customer_address);
@@ -56,6 +63,20 @@ public class PickupEntryActivity extends AppCompatActivity {
         order_entry_layout.setVisibility(View.GONE);
         load_pickup_layout_manager = new LinearLayoutManager(this);
         load_pickups_recycle.setLayoutManager(load_pickup_layout_manager);
+        old_pickup.setVisibility(View.GONE);
+        new_pickup.setVisibility(View.GONE);
+
+        phone_call = findViewById(R.id.billing_phone5);
+        facebook = findViewById(R.id.billing_fb);
+        my_delivery = findViewById(R.id.billing_my_delivery);
+        dashboard_billing = findViewById(R.id.billing_Billing);
+        settings = findViewById(R.id.billing_settings);
+        user_manual = findViewById(R.id.billing_user_manual);
+        log_out = findViewById(R.id.billing_logout);
+        dhep_delivery = findViewById(R.id.billing_dheo_delivery);
+        the_user_manual = findViewById(R.id.billing_The_manual);
+        meet_the_team = findViewById(R.id.billing_meet_team);
+        privacy_policy = findViewById(R.id.billing_policy);
 
         getSupportActionBar().setElevation(0);//remove actionbar shadow
         setTitle("My Pickup Orders");
@@ -68,6 +89,26 @@ public class PickupEntryActivity extends AppCompatActivity {
         });
 
         loadPickupEntry();
+        old_pickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(load_pickup_remaining - (load_pickup_remaining/(page_number*8)) > 0){
+                    page_number++;
+                    loadPickupEntry();
+                }
+            }
+        });
+
+        new_pickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(page_number > 0){
+                    page_number--;
+                    loadPickupEntry();
+                }
+            }
+        });
+
         cancel_entry_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,7 +157,7 @@ public class PickupEntryActivity extends AppCompatActivity {
                 }
                 date = String.valueOf(entry_datePicker.getDayOfMonth()) +" " + month + " "+ String.valueOf(entry_datePicker.getYear());
                 if(entry_customer_name.getText().length() > 0 && entry_customer_address.getText().length() > 0 && entry_customer_phone.getText().length() == 11 && entry_customer_cod.getText().length() > 0){
-                    Toast.makeText(getApplicationContext(), date, Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(), date, Toast.LENGTH_LONG).show();
                     Call<ResponseBody> entryCall = RetrofitClient
                             .getInstance()
                             .getApi()
@@ -158,6 +199,127 @@ public class PickupEntryActivity extends AppCompatActivity {
 //                startActivity(intent);
             }
         });
+
+        Call<PickupOrders> pickupOrdersCall = RetrofitClient
+                .getInstance()
+                .getApi()
+                .load_pickup_orders(client_id, page_number);
+        pickupOrdersCall.enqueue(new Callback<PickupOrders>() {
+            @Override
+            public void onResponse(Call<PickupOrders> call, Response<PickupOrders> response) {
+                if(response.body() != null){
+                    if(response.body().getM().size() < 2){
+                        recycle_layout_view.setVisibility(View.GONE);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PickupOrders> call, Throwable t) {
+
+            }
+        });
+        phone_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel: 09613533533"));
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://m.me/dheolife";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
+        my_delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ClientDashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+        dashboard_billing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ClientDashboardBillingActivity.class);
+                startActivity(intent);
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+        user_manual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://rocket.dheo.com/user-manual";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+                editor = sharedPreferences.edit();
+                editor.putBoolean("saveLogin", false);
+                editor.commit();
+                Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+        dhep_delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ClientDashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+        the_user_manual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://rocket.dheo.com/user-manual";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+        meet_the_team.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://team.dheo.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+        privacy_policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://dheo.com/privacy";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
 
     }
 
